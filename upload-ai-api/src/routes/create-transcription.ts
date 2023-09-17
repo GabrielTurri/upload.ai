ff829916c76cb059c206c263b93e797b83616b7c
Gabriel Turri <ga-briel.t.a@hotmail.com>
2023-09-17 14:05:42 -0300

    using openai api

--- upload-ai-api/src/routes/create-transcription.ts
+++ upload-ai-api/src/routes/create-transcription.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from "fastify"
+import { createReadStream } from "node:fs"
 import { prisma } from "../lib/prisma"
 import { z } from 'zod'
+import { openai } from "../lib/openai"
 
 export async function createTranscriptionRoute(app: FastifyInstance) {
   app.post('/videos/:videoId/transcription', async (req) => {
@@ -23,10 +25,28 @@ export async function createTranscriptionRoute(app: FastifyInstance) {
     })
     
     const videoPath = video.path
+    const audioReadStream = createReadStream(videoPath)
 
-    return {
-      videoId,
-      prompt
-    }
+    const response = await openai.audio.transcriptions.create({
+      file: audioReadStream,
+      model: 'whisper-1',
+      language: 'pt',
+      response_format: 'json',
+      temperature: 0,
+      prompt,
+    })
+
+    const transcription = response.text
+
+    await prisma.video.update({
+      where: {
+        id: videoId
+      },
+      data: {
+        transcription
+      },
+    })
   })
+
+  return { transcription }
 }
